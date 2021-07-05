ccd44ebdd71aa91ccd4055f53bd72218bd859eb4
Bartha Vince Döme <bartha.vince.dome@gmail.com>
2021-07-05 16:26:28 +0200

    actually no syntax error

--- amy.py
+++ amy.py
@@ -5,7 +5,7 @@ from discord.utils import get
 
 
 path_to_tokenL = os.getcwd().split("\\")
-if len path_to_tokenL == 1:
+if len(path_to_tokenL) == 1:
     path_to_tokenL = os.getcwsd().split("/")
 del path_to_tokenL[len(path_to_tokenL)-1]
 
